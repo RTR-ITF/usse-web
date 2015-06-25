d782ba5412da31d42c6fcb3454e7da3202dc002e
Jawadh-Salih <jawadhsalih.4th@gmail.com>
2015-06-25 22:12:49 +0530

    Structure is done

--- other-clients.php
+++ other-clients.php
@@ -1,134 +1,178 @@
 <?php include "includes/header.php" ?>
 
 <body>
-
     <!-- Navigation -->
-    <?php include "includes/nav-bar.php" ?>
-    
+        <?php include "includes/nav-bar.php" ?>
+
+
     <!-- Page Content -->
     <div class="container">
 
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Blog Home Two
-                    <small>Subheading</small>
-                </h1>
-                <ol class="breadcrumb">
-                    <li><a href="index.html">Home</a>
-                    </li>
-                    <li class="active">Blog Home Two</li>
-                </ol>
-            </div>
-        </div>
-        <!-- /.row -->
-
-        <!-- Blog Post Row -->
-        <div class="row">
-            <div class="col-md-1 text-center">
-                <p><i class="fa fa-camera fa-4x"></i>
-                </p>
-                <p>June 17, 2014</p>
-            </div>
-            <div class="col-md-5">
-                <a href="blog-post.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/600x300" alt="">
-                </a>
-            </div>
-            <div class="col-md-6">
-                <h3>
-                    <a href="blog-post.html">Blog Post Title</a>
-                </h3>
-                <p>by <a href="#">Start Bootstrap</a>
-                </p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                <a class="btn btn-primary" href="blog-post.html">Read More <i class="fa fa-angle-right"></i></a>
-            </div>
-        </div>
-        <!-- /.row -->
-
-        <hr>
-
-        <!-- Blog Post Row -->
-        <div class="row">
-            <div class="col-md-1 text-center">
-                <p><i class="fa fa-film fa-4x"></i>
-                </p>
-                <p>June 17, 2014</p>
-            </div>
-            <div class="col-md-5">
-                <a href="blog-post.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/600x300" alt="">
-                </a>
-            </div>
-            <div class="col-md-6">
-                <h3><a href="blog-post.html">Blog Post Title</a>
-                </h3>
-                <p>by <a href="#">Start Bootstrap</a>
-                </p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                <a class="btn btn-primary" href="blog-post.html">Read More <i class="fa fa-angle-right"></i></a>
-            </div>
-        </div>
-        <!-- /.row -->
-
-        <hr>
-
-        <!-- Blog Post Row -->
-        <div class="row">
-            <div class="col-md-1 text-center">
-                <p><i class="fa fa-file-text fa-4x"></i>
-                </p>
-                <p>June 17, 2014</p>
-            </div>
-            <div class="col-md-5">
-                <a href="blog-post.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/600x300" alt="">
-                </a>
-            </div>
-            <div class="col-md-6">
-                <h3><a href="blog-post.html">Blog Post Title</a>
-                </h3>
-                <p>by <a href="#">Start Bootstrap</a>
-                </p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                <a class="btn btn-primary" href="blog-post.html">Read More <i class="fa fa-angle-right"></i></a>
-            </div>
-        </div>
-        <!-- /.row -->
-
-        <hr>
-
-        <!-- Pager -->
-        <div class="row">
-            <ul class="pager">
-                <li class="previous"><a href="#">&larr; Older</a>
-                </li>
-                <li class="next"><a href="#">Newer &rarr;</a>
-                </li>
-            </ul>
-        </div>
-        <!-- /.row -->
-
-        <hr>
-
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="well">
+                    <h4>Search</h4>
+                    <div class="input-group">
+                        <input type="text" class="form-control">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
+                        </span>
+                    </div>
+                    <!-- /.input-group -->
                 </div>
-            </div>
-        </footer>
+        <div class="row">
+                <h1 class="page-header"> Other Clients
+                                    <img class="img-responsive img-hover" src="http://placehold.it/1170x300" alt="">
 
-    </div>
-    <!-- /.container -->
+<!--                     <small>Subheading</small>
+ -->                </h1>
+        </div>
+        <!-- /.row -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+        <div class="row">
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+            <!-- Blog Entries Column -->
+
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#">Client 1</a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"></a>
+                </p>
+                <p><i class="fa fa-clock-o"></i> Project name or a title with the client</p>
+                <hr>
+                <a href="blog-post.html">
+                    <img class="img-responsive img-hover" src="http://placehold.it/900x300" alt="">
+                </a>
+                <hr>
+                <p> Description about the project that has beem done or doing currently after the read more button is presses. With a java script effect</p> 
+                <a class="btn btn-primary" href="#">Read More <i class="fa fa-angle-right"></i></a>
+
+                <hr>
+
+                <!-- Second Blog Post -->
+               
+
+                <!-- Third Blog Post -->
+                <h2>
+                    <a href="#">Client 2</a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"></a>
+                </p>
+                <p><i class="fa fa-clock-o"></i> Project name or a title with the client</p>
+                <hr>
+                <a href="blog-post.html">
+                    <img class="img-responsive img-hover" src="http://placehold.it/900x30" alt="">
+                </a>
+                <hr>
+                <p> Description about the project that has beem done or doing currently</p> 
+                <a class="btn btn-primary" href="#">Read More <i class="fa fa-angle-right"></i></a>
+
+                <hr>                
+
+
+            <!-- Blog Sidebar Widgets Column -->
+            <div class="col-md-4">
+
+                <!-- Blog Search Well -->
+                
+
+                <!-- Blog Categories Well -->
+                
+        <!-- Footer -->
+
 
 </body>
+    <?php include "includes/footer.php" ?>
+
+</html>
+<?php include "includes/header.php" ?>
+
+<body>
+    <!-- Navigation -->
+        <?php include "includes/nav-bar.php" ?>
+
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="well">
+                    <h4>Search</h4>
+                    <div class="input-group">
+                        <input type="text" class="form-control">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
+                        </span>
+                    </div>
+                    <!-- /.input-group -->
+                </div>
+        <div class="row">
+                <h1 class="page-header"> Other Clients
+                                    <img class="img-responsive img-hover" src="http://placehold.it/1170x300" alt="">
+
+<!--                     <small>Subheading</small>
+ -->                </h1>
+        </div>
+        <!-- /.row -->
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#">Client 1</a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"></a>
+                </p>
+                <p><i class="fa fa-clock-o"></i> Project name or a title with the client</p>
+                <hr>
+                <a href="blog-post.html">
+                    <img class="img-responsive img-hover" src="http://placehold.it/900x300" alt="">
+                </a>
+                <hr>
+                <p> Description about the project that has beem done or doing currently after the read more button is presses. With a java script effect</p> 
+                <a class="btn btn-primary" href="#">Read More <i class="fa fa-angle-right"></i></a>
+
+                <hr>
+
+                <!-- Second Blog Post -->
+               
+
+                <!-- Third Blog Post -->
+                <h2>
+                    <a href="#">Client 2</a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"></a>
+                </p>
+                <p><i class="fa fa-clock-o"></i> Project name or a title with the client</p>
+                <hr>
+                <a href="blog-post.html">
+                    <img class="img-responsive img-hover" src="http://placehold.it/900x30" alt="">
+                </a>
+                <hr>
+                <p> Description about the project that has beem done or doing currently</p> 
+                <a class="btn btn-primary" href="#">Read More <i class="fa fa-angle-right"></i></a>
+
+                <hr>                
+
+
+            <!-- Blog Sidebar Widgets Column -->
+            <div class="col-md-4">
+
+                <!-- Blog Search Well -->
+                
+
+                <!-- Blog Categories Well -->
+                
+        <!-- Footer -->
+
+
+</body>
+    <?php include "includes/footer.php" ?>
 
 </html>
