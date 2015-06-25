f7f9172b41a7bf8e833cb5c7f144dab6d47d9f57
Kasun Dissanayake <kasun.bdn@gmail.com>
2015-06-25 07:32:18 +0530

    map

--- contact.php
+++ contact.php
@@ -27,21 +27,54 @@
             <!-- Map Column -->
             <div class="col-md-8">
                 <!-- Embedded Google Map -->
-                <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
+                <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" <iframe width="420" height="345" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Ulukade+Junction+Rd,+Sri+Lanka&amp;aq=0&amp;oq=ul&amp;sll=37.0625,-95.677068&amp;sspn=56.506174,79.013672&amp;t=m&amp;ie=UTF8&amp;hq=&amp;hnear=Sathutu+Uyana,+Ganemulla,+Gampaha,+Western+Province,+Sri+Lanka&amp;ll=7.05775,79.959522&amp;spn=0.008763,0.009645&amp;z=14&amp;iwloc=lyrftr:m,1628981885762990429,7.060264,79.948379&amp;output=embed"></iframe>
             </div>
             <!-- Contact Details Column -->
             <div class="col-md-4">
-                <h3>Contact Details</h3>
-                <p>
-                    3481 Melrose Place<br>Beverly Hills, CA 90210<br>
-                </p>
-                <p><i class="fa fa-phone"></i> 
-                    <abbr title="Phone">P</abbr>: (123) 456-7890</p>
-                <p><i class="fa fa-envelope-o"></i> 
-                    <abbr title="Email">E</abbr>: <a href="mailto:name@example.com">name@example.com</a>
-                </p>
-                <p><i class="fa fa-clock-o"></i> 
-                    <abbr title="Hours">H</abbr>: Monday - Friday: 9:00 AM to 5:00 PM</p>
+
+
+              <div class= contact-item>
+        					<h3>Head office & Factory</h3>
+        					<p>USS Engineering (Pvt) Ltd.<br>
+        							397, Galahitiyawa South,<br>
+        							Ganemulla, Sri Lanka<br>
+        					</p>
+        					<p><i class="fa fa-phone"></i>
+        						<abbr title="Phone">P</abbr>: +94332260066   <br>     +94335628911 - 3 </p>
+        					<p><i class="fa fa-envelope-o"></i>
+        						<abbr title="Email">E</abbr>: <a href="mailto:info@ussengineering.com">info@ussengineering.com</a>
+        					</p>
+
+
+        			</div>
+
+
+        			<div class= contact-item>
+        					<h3>Pole yard</h3>
+        					<p>USS Engineering (Pvt) Ltd.<br>
+        							397, Galahitiyawa South,<br>
+        							Ganemulla, Sri Lanka<br>
+        					</p>
+        					<p><i class="fa fa-phone"></i> 
+        						<abbr title="Phone">P</abbr>: +94332260066   <br>     +94335628911 - 3 </p>
+        					<p><i class="fa fa-envelope-o"></i>
+        						<abbr title="Email">E</abbr>: <a href="mailto:info@ussengineering.com">info@ussengineering.com</a>
+        					</p>
+        			</div>
+
+        			<div class= contact-item>
+        					<h3>Contact Details</h3>
+        					<p>USS Engineering (Pvt) Ltd.<br>
+        							397, Galahitiyawa South,<br>
+        							Ganemulla, Sri Lanka<br>
+        					</p>
+        					<p><i class="fa fa-phone"></i>
+        						<abbr title="Phone">P</abbr>: +94332260066   <br>     +94335628911 - 3 </p>
+        					<p><i class="fa fa-envelope-o"></i>
+        						<abbr title="Email">E</abbr>: <a href="mailto:info@ussengineering.com">info@ussengineering.com</a>
+        					</p>
+        			</div>
+
                 <ul class="list-unstyled list-inline list-social-icons">
                     <li>
                         <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
