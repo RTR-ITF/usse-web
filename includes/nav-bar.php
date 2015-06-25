c4742137541630bd911e5cb66812fd3553b0c42c
Jawadh-Salih <jawadhsalih.4th@gmail.com>
2015-06-25 13:23:28 +0530

    Please pull this and Do the rest

--- includes/nav-bar.php
+++ includes/nav-bar.php
@@ -19,9 +19,7 @@
                     <li>
                         <a href="services.php">Services</a>
                     </li>
-                    <li>
-                        <a href="contact.php">Contact</a>
-                    </li>
+                   
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Portfolio <b class="caret"></b></a>
                         <ul class="dropdown-menu">
@@ -53,25 +51,11 @@
                             </li>
                         </ul>
                     </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Other Pages <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a href="full-width.php">Full Width Page</a>
-                            </li>
-                            <li>
-                                <a href="sidebar.php">Sidebar Page</a>
-                            </li>
-                            <li>
-                                <a href="faq.php">FAQ</a>
-                            </li>
-                            <li>
-                                <a href="404.php">404</a>
-                            </li>
-                            <li>
-                                <a href="pricing.php">Pricing Table</a>
-                            </li>
-                        </ul>
+                    <li>
+                        <a href="news-events.php">New & Events</a>
+                    </li>
+                     <li>
+                        <a href="contact.php">Contact us</a>
                     </li>
                 </ul>
             </div>
