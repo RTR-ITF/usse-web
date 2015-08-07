d466498612e3dc48d7136c95c355cc1b5e0a722d
Jawadh-Salih <jawadhsalih.4th@gmail.com>
2015-08-07 21:01:07 +0530

    Directors

--- about.php
+++ about.php
@@ -34,68 +34,152 @@
         <div class="row">
             <div class="col-lg-12">
                 <h2 class="page-header">Board of Directors</h2>
-            </div>
-            <div class="col-md-4 text-center">
-                <div class="thumbnail">
-                    <img class="img-responsive" src="http://placehold.it/750x450" alt="">
-                    <div class="caption">
-                        <h3>Name of the memeber<br>
-                            <small>Job Title</small>
-                        </h3>
-                        <p> Description about the person.</p>
-                        <ul class="list-inline">
-                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
-                            </li>
-                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
-                            </li>
-                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
-                            </li>
-                        </ul>
+                <div class="col-md-4 text-center">
+                    <div class="thumbnail">
+                        <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                        <div class="caption">
+                            <h3>Mr. K.A.U. Thalangama<br>
+                                <small>Chariman/Director</small>
+                            </h3>
+                            <p> Description about the person.</p>
+                            <ul class="list-inline">
+                                <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4 text-center">
-                <div class="thumbnail">
-                    <img class="img-responsive" src="http://placehold.it/750x450" alt="">
-                    <div class="caption">
-                        <h3>Name of the memeber<br>
-                            <small>Job Title</small>
-                        </h3>
-                        <p> Description about the person.</p>
-                        <ul class="list-inline">
-                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
-                            </li>
-                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
-                            </li>
-                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
-                            </li>
-                        </ul>
+                <div class="col-md-4 text-center">
+                    <div class="thumbnail">
+                        <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                        <div class="caption">
+                            <div class="caption">
+                                <h3>Mrs. A.B.C.P. Thalangama<br>
+                                    <small>Director</small>
+                                </h3>
+                                <p> Description about the person.</p>
+                                <ul class="list-inline">
+                                    <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                                    </li>
+                                    <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                                    </li>
+                                    <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="col-md-4 text-center">
+                    <div class="thumbnail">
+                        <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                        <div class="caption">
+                            <h3>Mr. Vinal Thalagalar<br>
+                                <small>Director (Administration)</small>
+                            </h3>
+                            <p> Description about the person.</p>
+                            <ul class="list-inline">
+                                <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4 text-center">
-                <div class="thumbnail">
-                    <img class="img-responsive" src="http://placehold.it/750x450" alt="">
-                    <div class="caption">
-                       <div class="caption">
-                        <h3>Name of the memeber<br>
-                            <small>Job Title</small>
-                        </h3>
-                        <p> Description about the person.</p>
-                        <ul class="list-inline">
-                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
-                            </li>
-                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
-                            </li>
-                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
-                            </li>
-                        </ul>
+                <div class="col-md-4 text-center">
+                    <div class="thumbnail">
+                        <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                        <div class="caption">
+                            <h3>Mr. P.A. Wimalasiri<br>
+                                <small>Director</small>
+                            </h3>
+                            <p> Description about the person.</p>
+                            <ul class="list-inline">
+                                <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
+                <div class="col-md-4 text-center">
+                    <div class="thumbnail">
+                        <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                        <div class="caption">
+                            <div class="caption">
+                                <h3>Mr. R.M.Gunasekara<br>
+                                    <small>ACA,BA,DPFM,FMAAT,Attorney-at-Law</small>
+                                    <small>Director (Finance)</small>
+                                </h3>
+                                <p> Description about the person.</p>
+                                <ul class="list-inline">
+                                    <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                                    </li>
+                                    <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                                    </li>
+                                    <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 text-center">
+                        <div class="thumbnail">
+                            <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                            <div class="caption">
+                                <h3>Dr. Mahesh Jayaweera<br>
+                                    <small>BSc.(Eng),PhD in Eng., C.Eng</small>
+                                    <small>Director (Technical)</small>
+                                </h3>
+                                <p> Description about the person.</p>
+                                <ul class="list-inline">
+                                    <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                                    </li>
+                                    <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                                    </li>
+                                    <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-            
-            
-        </div>
+            <div class="col-lg-12">
+                <h2 class="page-header">Consultants</h2>
+                <div class="col-md-4 text-center">
+                    <div class="thumbnail">
+                        <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                        <div class="caption">
+                            <h3>Eng. Gamini Senanayaka<br>
+                                <small>BSc.(Eng.)Hons,MBA,FIE(SL),MIE(Aust),C.Eng</small>
+                                <small>Business and Technical Advisor</small>
+                            </h3>
+                            <p> Description about the person.</p>
+                            <ul class="list-inline">
+                                <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
         </div>
         <!-- /.row -->
 
