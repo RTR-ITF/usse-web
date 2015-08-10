267d53e767fce94a198e02d43c97145860dfacc8
Kasun Dissanayake <kasun.bdn@gmail.com>
2015-08-10 23:14:54 +0530

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,19 +1,68 @@
             
-
+</div>
+</div>
         <!-- Footer -->
-        <div  id="footer">
-		     
-		                 <div class="navbar navbar-default navbar-bottom">
-                            <div >
-                              <p class="navbar-text pull-left">copyright © 2015 
-                                   <a href="#" target="_blank"> USS Engineering(Pvt)Ltd. </a> All rights reserved.
-                              </p>
+		<footer class="footer-distributed">
 
-                            </div>
+			<div class="footer-left">
 
-                        </div>
-		         
-		</div>
+				<h3>Company<span>logo</span></h3>
+
+				<p class="footer-links">
+					<a href="#">Home</a>
+					·
+					<a href="#">Blog</a>
+					·
+					<a href="#">Pricing</a>
+					·
+					<a href="#">About</a>
+					·
+					<a href="#">Faq</a>
+					·
+					<a href="#">Contact</a>
+				</p>
+
+				<p class="footer-company-name">Company Name &copy; 2015</p>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>21 Revolution Street</span> Paris, France</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+1 555 123456</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:support@company.com">support@company.com</a></p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>About the company</span>
+					Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+				</p>
+
+				<div class="footer-icons">
+
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-linkedin"></i></a>
+					<a href="#"><i class="fa fa-github"></i></a>
+
+				</div>
+
+			</div>
+
+		</footer>
 
     <!-- /.container -->
 
@@ -25,11 +74,11 @@
 	<script src="js/jquery.slimmenu.js"></script>
 	
 	<script >
-     $(window).load(function() {
-     $('#loading').hide();
-	 document.getElementById("slider").style.display = "block";
-  });
-</script>
+		 $(window).load(function() {
+		 $('#loading').hide();
+		 document.getElementById("slider").style.display = "block";
+		});
+	</script>
 
     <!-- Script to Activate the Carousel -->
     <script>
@@ -40,16 +89,15 @@
 	</script>
 	
 	<script>
-	$('#navigation').slimmenu(
-{
-    resizeWidth: '1000',
-    collapserTitle: '',
-    animSpeed: 'medium',
-    easingEffect: 'linear',
-    indentChildren: false,
-    childrenIndenter: '&nbsp;'
-});
-
+		$('#navigation').slimmenu(
+		{
+			resizeWidth: '1000',
+			collapserTitle: '',
+			animSpeed: 'medium',
+			easingEffect: 'linear',
+			indentChildren: false,
+			childrenIndenter: '&nbsp;'
+		});
 
     </script>
  
