5bc1253c30a8831f7c27b60b1bd2481df8c48ae1
Kasun Dissanayake <kasun.bdn@gmail.com>
2015-10-07 21:22:14 +0530

    main page css

--- index.php
+++ index.php
@@ -18,34 +18,34 @@
             <div class="col-md-4">
                 <div class="panel panel-default short-description">
                     <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-check"></i> Need to know Who are We ?</h4>
+                        <h3>Need to know Who are We ?</h3>
                     </div>
                     <div class="panel-body">
                         <p> USS Engineering ( Pvt ) Ltd possesses expertise in a vast range of engineering fields, mainly in 
                         the Manufacture, Fabrication and Galvanizing of steel products </p> 
-                        <a href="about.php" class="btn btn-default">Need to know  Further ?</a>
+                        <a href="about.php" ><h4>Need to know  Further ?</h4></a>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="panel panel-default short-description">
                     <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-check"></i>Our Potential</h4>
+                        <h3>Our Potential</h3>
                     </div>
                     <div class="panel-body">
                         <p> We are not only a single purpose company, Our potential is diversed. Click below for our Products</p>
-                        <a href="services.php" class="btn btn-default">Our Services</a>
+                        <a href="services.php" ><br><h4>Our Services</h4></a>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="panel panel-default short-description">
                     <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-check"></i> What have we done</h4>
+                        <h3>What have we done ?</h3>
                     </div>
                     <div class="panel-body">
                         <p> Major Achievements of the company are described </p>
-                        <a href="news-events.php" class="btn btn-default"> More on our products.</a>
+                        <a href="news-events.php" ><br><br><h4> More on our products.</h4></a>
                     </div>
                 </div>
             </div>
