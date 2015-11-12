67eb66ba23e78d7c11ea24c9b9d149f3c08250f7
Jawadh-Salih <jawadhsalih.4th@gmail.com>
2015-11-12 23:33:04 +0530

    Allignment of paragraphs are changed

--- services.php
+++ services.php
@@ -39,7 +39,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>Steel Hardware Products</h4>
-                        <p style="text-align: justify">
+                        <p style="text-align: initial">
                         We manufacture an assortment exquite steel hardware products, which are widely acclaimed for their superior 
                         quality and hi-tech desings. These items are high demand within many Government Institutions such as CEB, SLT,
                         and LECO. We are unique in our capability of manufacturing all types of steel hardware products as per ay specifications </p>
@@ -53,7 +53,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>Steel Fabrication</h4>
-                        <p style="text-align: justify">
+                        <p style="text-align: initial">
                             At USS Engineering Steel Fabrication unitm we are fully committed to accomplishment of every project undertaken 
                             by us, thus guaranteeing customer satisfaction. We are proud of and are honoured by the trust placed in us by 
                             our customers. Our hallmark of success in steel fabrication in Sri Lanka, is attributable to our experience, 
@@ -69,7 +69,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>Concrete Products</h4>
-                        <p style="text-align: justify">
+                        <p style="text-align: initial">
                         Our RC Concrete Poles manufacturing unit is playing a major role. We are the first choice of CEB, LECO and SLT. During
                         past 2 decades we have delivered  concrete poles with suoerior quality and we have our own manfacturing yards islandwide
                         with attractive prices. And all our concrete poles are customized and made to order. 
@@ -84,7 +84,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>LED Bulbs</h4>
-                        <p style="text-align: justify">
+                        <p style="text-align: initial">
                             TOMA LIGHT our new product for Sri Lanka. This LED tube lights are currently producing in Japan and we are the agent 
                             and distributors of this product. And in near furture we planned to install a manufaturing plant of this product.                            
                         </p>
@@ -103,7 +103,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>Buildings</h4>
-                        <p style="text-align: justify">
+                        <p style="text-align: initial">
                             We construct sophisticated, elegent structures to the highest standard. Our emphasis is on accuracy, quality and customer
                             satisfaction. Since our past services to the building construction, today we are highly respected by our clientele. We are
                             fully geared to take on the requirements of our clients, with integrity, passion for architecture and high standards, in 
@@ -119,7 +119,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>Highways</h4>
-                        <p style="text-align: justify">
+                        <p style="text-align: initial">
                             Earth work, Structural work - Culverts, Bridges, Line drains, Causeways, Irrigation structures
                             Asphalt Laying, DBST Laying, Road Making and Installation of Sign Boards etc.
                         </p>
@@ -133,7 +133,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>Power Transmission Lines</h4>
-                        <p style="text-align: justify">
+                        <p style="text-align: initial">
                          Laying 132kV and 33kV in various parts of the Island.
                         </p>
                     </div>
@@ -146,7 +146,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>Water Supply & Drainage</h4>
-                        <p style="text-align: justify">
+                        <p style="text-align: initial">
                         Pipe Laying, Construction of Dams and Water treatment plants, Construction of reservoirs, Constructions 
                         of Sewerage and Drainage lines are our major water supply services.
                         </p>
@@ -160,7 +160,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>Telecommunication Engineering & Construction Work</h4>
-                        <p style="text-align: justify">
+                        <p style="text-align: initial">
                          In Colombo  we have established our Telecommunication Division in 2003. We are fully geared now to undertake
                          any type of telecommunication project in any part of the country. ALso we now rank as a Reputed Registered Unit
                          Rate Contractor of SLT, and successfully accomplished the "Deyata Kirula" Government Project at Buttala.
@@ -175,7 +175,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>Electrical Engieering & Construction</h4>
-                        <p style="text-align: justify">
+                        <p style="text-align: initial">
                         With the guide of experienced professionals our organization has engineered a variety of notable projects 
                         for the Government and the private sector. We undertake any project upon careful assessment of the client's 
                         rewuirement, in order to deliver the best solution.
@@ -190,7 +190,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>Design and Consultancy Services</h4>
-                        <p style="text-align: justify">
+                        <p style="text-align: initial">
                         With the vast experience our consultants we have newly opened our consultancy division to assist 
                         any project in the fields of water, wastewater and electricity. Our consultants offer services and 
                         methodologies designed to address technological needs, ranging from strategic planning to rapid implementation
@@ -212,7 +212,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>Railway Items</h4>
-                        <p style="text-align: justify">
+                        <p style="text-align: initial">
                         Rolls, Brake pads, Connectors, Insulators, Sleeper pads, Rubber pads, Fish plates, Junction fish plates and Rail Clips
                         are out main supplies to the railway.
                         </p>
@@ -227,7 +227,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>Electrical Accessories</h4>
-                        <p style="text-align: justify">
+                        <p style="text-align: initial">
                         Transformers, Surge Arrestors, Porcelain Insulators, Electrical overload  protection accessories
                         Cut-outs, Connectors, Pin connectors, Solar Panels, Fuses are major supplies which are imported.
                         </p>
@@ -241,7 +241,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>Water Treatment and Supplies</h4>
-                        <p style="text-align: justify">
+                        <p style="text-align: initial">
                         Water meters, Chemicals (Aluminium Sulphate), PAC, Hydrated Lime, Bleaching powder, HDPE/DI pipes, Valves, Pumps
                         ,Filters, Gun metal ferrules, Water Meter Test Bench, Chlorine gas,GI pipes are the supplied under water treatment 
                         items division.
@@ -256,7 +256,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>Steel Items</h4>
-                        <p style="text-align: justify">
+                        <p style="text-align: initial">
                         We supply steel for Steel Towers, GI pipes, Clamps and Brackets, Plastics Security seals, Electricity meter Seals,
                         Telephone Units, Ladder, Bolt Seals, Sealing Straps.
                         </p>
@@ -270,7 +270,7 @@
                     </div>
                     <div class="panel-body">
                         <h4>Electrical & Telecom Cables</h4>
-                        <p style="text-align: justify">
+                        <p style="text-align: initial">
                         High/Low voltage underground or power cables, ACSR (Lynx, Racoon), AAC & AAAC conductors, Multi pair telephone cables,
                         Co-axial xables and Fiber opticals are imported and supplied Islandwide.
                         </p>
